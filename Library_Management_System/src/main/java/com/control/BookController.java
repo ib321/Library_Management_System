139cac666a35d8ab1c650bbb6a197f778096d7e6
ib321 <newtonbk123@gmail.com>
2022-02-20 11:50:18 +0530

    added BookBO class and done some changes in connection of files
    required to separate the business logic with servlet and DAO class

--- Library_Management_System/src/main/java/com/control/BookController.java
+++ Library_Management_System/src/main/java/com/control/BookController.java
@@ -1,9 +1,6 @@
 package com.control;
 
-
-
 import java.io.IOException;
-
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.dao.BookDAO;
+import com.bo.BookBO;
 import com.entity.Book;
 import com.exception.BookNotFoundException;
 import com.service.BookService;
@@ -29,61 +26,55 @@ public class BookController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-        BookService bks=new BookService();
-        BookDAO bkd = new BookDAO();
-        String print,search = null;
+
+		BookService bks = new BookService();
+		BookBO bkb = new BookBO();
+		String print, search = null;
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>This is Not Final Yet!!<br>:)</h1>");
-		List l=new ArrayList();
-		List ch=new ArrayList();
+		List l = new ArrayList();
+		List ch = new ArrayList();
 		String name = request.getParameter("book_name");
-		if(name!=null) {
+		if (name != null) {
 			search = name;
-			 l = bks.getBookByName(search);
-		out.println("<h1>Hello!!<br> Got it What You Entered <br> Book Name : <hr>"+name+"<h1>");
-		            }
+			l = bks.getBookByName(search);
+			out.println("<h1>Hello!!<br> Got it What You Entered <br> Book Name : <hr>" + name + "<h1>");
+		}
 		String Author = request.getParameter("author_name");
-		if(Author!=null)
-		{ 
-		 l = bkd.findByAuthor(Author);
-		out.println("<h1>Hello!!<br> Got it What You Entered <br> Author_name : <hr>"+Author+"<h1>");
+		if (Author != null) {
+			l = bks.getBookByAuthor(Author);
+			out.println("<h1>Hello!!<br> Got it What You Entered <br> Author_name : <hr>" + Author + "<h1>");
 		}
 		String category = request.getParameter("category");
-		if(category!=null)
-		{    l = bkd.findByCategory(category);
-			search=category;
-		out.println("<h1>Hello!!<br> Got it What You Entered <br> category : <hr>"+category+"<h1>");
+		if (category != null) {
+			l = bks.getBookByCategory(category);
+			search = category;
+			out.println("<h1>Hello!!<br> Got it What You Entered <br> category : <hr>" + category + "<h1>");
 		}
-		
-			
-			
-			try {
+
+		try {
 			if (l.isEmpty()) {
 				out.println("<h1>Not Available<h1><hr>");
 				throw new BookNotFoundException("Book Not Found in Database");
-			}
-			else {
+			} else {
 				out.println("<h2 align=\"middle\">Book Details</h2>");
-			    out.printf("%-15s %-15s %-15s %-15s %s<br>", "BookNo", "BookName", "Author", "Category", "Available");
+				out.printf("%-15s %-15s %-15s %-15s %s<br>", "BookNo", "BookName", "Author", "Category", "Available");
 				for (Object object : l) {
 					Book b = (Book) object;
-					boolean c = bks.checkAvailability(b.getReturnDate());
-					if (c == true)print = "Available";
-				    else print = "Unavailable";
-			    //out.println("<h3>"+ b.getId()+b.getName()+b.getAuthor()+b.getCategory()+print+"<h3>");
-			    out.printf("%-25s %-25s %-25s %-25s %s<br>", b.getId(), b.getName(), b.getAuthor(), b.getCategory(),print);
-					}}
+					boolean c = bkb.checkAvailability(b.getReturnDate());
+					if (c == true)
+						print = "Available";
+					else
+						print = "Unavailable";
+					
+			out.printf("%-25s %-25s %-25s %-25s %s<br>", b.getId(), b.getName(), b.getAuthor(), b.getCategory(),print);
+				}
 			}
-			catch(Exception e)
-			{
-				out.println(e);
-			}
-			
-
+		} catch (Exception e) {
+			out.println(e);
 		}
-		
+
 	}
 
-
+}

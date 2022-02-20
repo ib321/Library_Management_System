ca334f6a95db7fb9d8c3d31c8de366f3444b7bae
ib321 <newtonbk123@gmail.com>
2022-02-20 11:56:07 +0530

    Almost Done The Project using hibernate Only Output file needed

---
+++ Library_Management_System/src/main/java/com/dao/Main.java
@@ -0,0 +1,35 @@
+package com.dao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import com.bo.BookBO;
+import com.entity.Book;
+import com.service.BookService;
+
+public class Main {
+	 static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BookDAO bkd=new BookDAO();
+		BookBO bkb = new BookBO();
+		BookService bks=new BookService();
+		String print;
+		System.out.println("Enter Book:");
+		String name=br.readLine();
+		//bks.DisplayBookByName(name);
+		List l=bkd.findByCategory(name);
+		if(l==null) {System.out.println("Not Available"); System.exit(0);}
+		System.out.printf("%-15s %-15s %-15s %-15s %s\n","BookNo","BookName","Author","Category","Available");
+		for (Object object : l) {
+			Book b=(Book) object;
+			boolean c=bkb.checkAvailability(b.getReturnDate());
+			if(c==true) {print="Available";}else {print="Unavailable";}
+			System.out.printf("%-15s %-15s %-15s %-15s %s\n",b.getId(),b.getName(),b.getAuthor(),b.getCategory(),print);
+		}
+
+	}
+
+}
---
+++ Library_Management_System/src/main/java/com/exception/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.exception;
+
+public class BookNotFoundException extends Exception{
+    public BookNotFoundException(String msg) {
+        super(msg);
+    }
+}

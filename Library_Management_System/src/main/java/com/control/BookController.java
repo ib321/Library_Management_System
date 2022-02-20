67ef345877cd0d5005957e6416118dbd0d9338c0
ib321 <newtonbk123@gmail.com>
2022-02-21 00:25:18 +0530

    Designed The Otput in Table Format Also used Backgroung i think Project Done

--- Library_Management_System/src/main/java/com/control/BookController.java
+++ Library_Management_System/src/main/java/com/control/BookController.java
@@ -32,34 +32,40 @@ public class BookController extends HttpServlet {
 		String print, search = null;
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("<h1>This is Not Final Yet!!<br>:)</h1>");
+		out.println(
+				"<html><head><style>h1{color: #0ae312;}"
+				+ "table,th,td{width: 40%;background-color: #e3520a; border: 1px solid black;}"
+				+ "</style><link rel=\"stylesheet\" type=\"text/css\" href=\"input1.css\"></head>");
+		out.println("<body><center><h1 style=\"color:white;\">This is Not Final Yet!!:)</h1> ");
 		List l = new ArrayList();
 		List ch = new ArrayList();
 		String name = request.getParameter("book_name");
 		if (name != null) {
 			search = name;
 			l = bks.getBookByName(search);
-			out.println("<h1>Hello!!<br> Got it What You Entered <br> Book Name : <hr>" + name + "<h1>");
+			out.println("<h1>Hello!! Got it What You Entered  <br> Book Name :" + name + "</h1>");
 		}
 		String Author = request.getParameter("author_name");
 		if (Author != null) {
 			l = bks.getBookByAuthor(Author);
-			out.println("<h1>Hello!!<br> Got it What You Entered <br> Author_name : <hr>" + Author + "<h1>");
+			out.println("<h1>Hello!! Got it What You Entered <br>Author Name :" + Author + "</h1>");
 		}
 		String category = request.getParameter("category");
 		if (category != null) {
 			l = bks.getBookByCategory(category);
 			search = category;
-			out.println("<h1>Hello!!<br> Got it What You Entered <br> category : <hr>" + category + "<h1>");
+			out.println("<h1>Hello!! Got it What You Entered <br>Category:<hr>" + category + "</h1>");
 		}
 
 		try {
 			if (l.isEmpty()) {
-				out.println("<h1>Not Available<h1><hr>");
+				out.println("<h1 style=\\\"color:white;\\>Not Available<h1><hr>");
 				throw new BookNotFoundException("Book Not Found in Database");
 			} else {
-				out.println("<h2 align=\"middle\">Book Details</h2>");
-				out.printf("%-15s %-15s %-15s %-15s %s<br>", "BookNo", "BookName", "Author", "Category", "Available");
+				out.println("<h2 style=\"color:white;\">Book Details</h2>");
+				out.printf(
+						"<table ><tr><th>BookNo</th><th>BookName</th> <th>Author</th> <th>Category</th><th>Available</th></tr></table>");
+
 				for (Object object : l) {
 					Book b = (Book) object;
 					boolean c = bkb.checkAvailability(b.getReturnDate());
@@ -67,14 +73,20 @@ public class BookController extends HttpServlet {
 						print = "Available";
 					else
 						print = "Unavailable";
-					
-			out.printf("%-25s %-25s %-25s %-25s %s<br>", b.getId(), b.getName(), b.getAuthor(), b.getCategory(),print);
+
+					out.println("<center><table><tr><td>" + b.getId() + "</td><td>" + b.getName() + "</td><td>"
+							+ b.getAuthor() + "</td><td>" + b.getCategory() + "</td><td>" + print
+							+ "</td></tr></table></center>");
 				}
 			}
 		} catch (Exception e) {
 			out.println(e);
 		}
+		out.println("</body></html>");
 
 	}
 
 }
+
+//out.printf("%-15s %-15s %-15s %-15s %s<br>", "BookNo", "BookName", "Author", "Category", "Available");
+//out.printf("%-25s %-25s %-25s %-25s %s<br>", b.getId(), b.getName(), b.getAuthor(), b.getCategory(),print);

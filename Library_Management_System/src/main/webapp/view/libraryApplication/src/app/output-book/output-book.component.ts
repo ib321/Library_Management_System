a2b3364c7a5398702e0dfa858970df42566c921a
ib321 <newtonbk123@gmail.com>
2022-03-27 12:42:51 +0530

    Done book not found exception in bookservice class Book not found implemented in angular with variable gif

--- Library_Management_System/src/main/webapp/view/libraryApplication/src/app/output-book/output-book.component.ts
+++ Library_Management_System/src/main/webapp/view/libraryApplication/src/app/output-book/output-book.component.ts
@@ -12,26 +12,28 @@ export class OutputBookComponent implements OnInit {
   	
   constructor(private bookService:BookService,private route: ActivatedRoute) { }
    Books:Book[]=[];
- 
+   length:number=0;
+ showResult:boolean=true;
   ngOnInit(): void {
 	  
 	this.Books=[];
 	this.route.params.subscribe( (parameters)=>{
 		
 		if (String(parameters['searchtype']).localeCompare('byname') == 0) {
-			this.bookService.findByName(String(parameters['bookname'])).subscribe(allbook=>this.Books=allbook);
-		
+			this.bookService.findByName(String(parameters['bookname'])).subscribe(allbook=>{this.Books=allbook;if(allbook.length==0)this.showResult=false;});
+					
 		} else if(String(parameters['searchtype']).localeCompare('byauthor') == 0){
-			this.bookService.findByAuthor(parameters['author']).subscribe(allbook=>this.Books=allbook);
+			this.bookService.findByAuthor(parameters['author']).subscribe(allbook=>{this.Books=allbook;if(allbook.length==0)this.showResult=false;});
 		
 		} else if(String(parameters['searchtype']).localeCompare('bycategory') == 0){
-			this.bookService.findByCategory(parameters['category']).subscribe(allbook=>this.Books=allbook);
+			this.bookService.findByCategory(parameters['category']).subscribe(allbook=>{this.Books=allbook;if(allbook.length==0)this.showResult=false;});
 		}
   });
 }
 
   public  checkAvailability( returnD:Date):String {
 		 let d = new Date();
+		
 		 let returnDate=new Date(returnD);
 		if (d < returnDate)
 			return "Unavailable";
@@ -43,4 +45,9 @@ export class OutputBookComponent implements OnInit {
 			return "Unavailable";
 	
 	}
+
+	 myPix = new Array("../../assets/gif/noresult.gif","../../assets/gif/noresult2.gif","../../assets/gif/noresult3.gif","../../assets/gif/noresult4.gif");
+	 randomNum = Math.floor(Math.random() * this.myPix.length);
+	 src = this.myPix[this.randomNum];
+
 }
